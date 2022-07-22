856e15e8777b759aa4d1e80dd34ae9c669814185
Norman Herrera <norman.herrera@ymail.com>
2022-07-21 23:08:59 -0700

    added helpers for time and date format

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    formatTime: (date) => {
+      return date.toLocaleTimeString();
+    },
+    formatDate: (date) => {
+      return date.toLocaleDateString();
+    },
+  };
